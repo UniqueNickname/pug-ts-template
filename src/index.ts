73e6ca32215fd928ca1284bffb85f53220882bb9
UniqueNickname <48050888+UniqueNickname@users.noreply.github.com>
2020-04-03 09:39:44 +0600

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+//!! Only for import (scss, images, fonts, libs etc) 
+
+//++ Scss                                            
+import './assets/scss/main.scss'
+
+//++ Images                                          
+//?? import './assets/img/logo.png'
+
+//++ Fonts                                           
+//?? import './assets/fonts/beermoney/beermoney-regular.woff2'
+
+//++ JS libs                                         
